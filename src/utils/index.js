c9d2dc7b9cf5f7c64f26e47147664aeff72b0f70
Dominik Kaegi <kaegidominik@gmail.com>
2020-02-19 17:54:21 -0300

    add delay to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,31 +2,44 @@ import * as userApi from "api/api.user";
 import * as apiTweet from "api/apit.tweet";
 import * as authApi from "api/api.auth";
 
+import { sleep } from "api/helpers";
+const customSleep = async () => {
+  const sleepTime = 200;
+  await sleep(sleepTime);
+};
+
 export async function getUsers() {
+  await customSleep();
   return userApi.getUsers();
 }
 
 export async function signup({ firstName, lastName, email, password }) {
+  await customSleep();
   return authApi.signup({ firstName, lastName, email, password });
 }
 
 export async function login({ email, password }) {
+  await customSleep();
   return authApi.login({ email, password });
 }
 
 export async function logout() {
+  await customSleep();
   return authApi.logout();
 }
 
 export async function getActiveUser() {
+  await customSleep();
   return userApi.getActiveUser();
 }
 
 export async function getUserById(id) {
+  await customSleep();
   return userApi.getUserById(id);
 }
 
 export async function createTweet({ message, user }) {
+  await customSleep();
   return apiTweet.create({ message, user });
 }
 
